ac0bc178d2565b236e73c97747fb49a17d45c862
Monu Prajapat <monuprajapat6270@gmail.com>
2024-03-03 21:18:41 +0530

    added cors

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cartRoute = require('./routes/cartRoute.js');
-const historyRoute = require('./routes/historyRoute.js');
-const reviewRoute = require('./routes/reviewRoute.js');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cartRoute = require("./routes/cartRoute.js");
+const historyRoute = require("./routes/historyRoute.js");
+const reviewRoute = require("./routes/reviewRoute.js");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const Port = process.env.Port || 5000;
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   next();
-})
+});
 
-app.use('/Cart', cartRoute);
-app.use('/History', historyRoute);
-app.use('/Main', reviewRoute);
+app.get("/", (request, response) => {
+  response.send(
+    `<h2>Welcome to the server of Eating Website</h2>`
+  );
+});
 
-mongoose.connect('mongodb+srv://pallavsingh07:pallavsingh07@cluster0.sf3fvsx.mongodb.net/Cluster0').then(() => { app.listen(Port) }).catch(err => { console.log(err) })
+app.use("/Cart", cartRoute);
+app.use("/History", historyRoute);
+app.use("/Main", reviewRoute);
+
+mongoose
+  .connect(
+    "mongodb+srv://pallavsingh07:pallavsingh07@cluster0.sf3fvsx.mongodb.net/Cluster0"
+  )
+  .then(() => {
+    app.listen(Port, () => {
+      console.log(`Node Js Server started at Port:${Port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
